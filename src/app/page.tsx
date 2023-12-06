8bbc671e64a32c28167690a026f1fd3cf37497bb
Vincent <141185796+Yalcael@users.noreply.github.com>
2023-12-06 18:08:50 +0100

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ const page = () => {
     <Center>
       <h1 className="text-3xl font-bold">Todo List</h1>
       <ul>
-        <li className="text-2xl font-bold"> text 1 </li>
-        <li className="text-2xl font-bold"> text 2 </li>
+        <li className="text-2xl font-bold mt-3"> text 1 </li>
+        <li className="text-2xl font-bold mt-3"> text 2 </li>
       </ul>
-      <input className="text-black text-xl font-bold" type="text" placeholder="Todo List" />
-      <button className="text-xl font-bold">Add</button>
+      <input className="text-black text-xl font-bold h-9 w-40 rounded-lg p-2 border focus:outline-none ml-2 mt-10" type="text" placeholder="Todo List" />
+      <button className="text-xl font-bold h-7 w-14 text-pink-700 rounded-md mt-2 border border-pink-700">Add</button>
     </Center>
   )
 }
