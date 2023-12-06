866ae757573c695cc214c721df2106947e560513
Vincent <141185796+Yalcael@users.noreply.github.com>
2023-12-06 15:45:59 +0100

    start project

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Todo List</h1>
+      <ul>
+        <li className="text-2xl font-bold"> text 1 </li>
+        <li className="text-2xl font-bold"> text 2 </li>
+      </ul>
+      <input type="text" placeholder="Todo List">
+      </input>
+    </div>
+  )
+}
+
+export default page
