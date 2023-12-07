7c79d0df487049f4187c71c557e55f997c99d30e
Vincent <141185796+Yalcael@users.noreply.github.com>
2023-12-07 16:37:15 +0100

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+import { useState } from "react";
 import Center from "@/components/Center"
 
-const page = () => {
+interface item {
+  id: number;
+  text: string;
+  done: boolean;
+}
+
+const Page: React.FC = () => {
+  const [todo, setTodo] = useState<item[]>([
+
+  ]);
+
+
   return (
     <Center>
       <h1 className="text-3xl font-bold">Todo List</h1>
@@ -9,9 +22,9 @@ const page = () => {
         <li className="text-2xl font-bold mt-3"> text 2 </li>
       </ul>
       <input className="text-black text-xl font-bold h-9 w-40 rounded-lg p-2 border focus:outline-none ml-2 mt-10" type="text" placeholder="Todo List" />
-      <button className="text-xl font-bold h-7 w-14 text-pink-700 rounded-md mt-2 border border-pink-700">Add</button>
+      <button className="text-xl font-bold h-7 w-14 text-pink-700 rounded-md mt-2 border border-pink-700 cursor-pointer">Add</button>
     </Center>
   )
 }
 
-export default page
+export default Page
